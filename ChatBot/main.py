1abf709511591171141ea68cbd033a55d0a95b3a
ryan <ryan.kimani0@gmail.com>
2025-07-02 19:58:12 +0800

    Manually Added Message History

--- ChatBot/main.py
+++ ChatBot/main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_openai import ChatOpenAI  
 
 # Load environment variables from .env file
@@ -17,9 +17,14 @@ model = ChatOpenAI(
     api_key=api_key,
     model="mistralai/mistral-7b-instruct",  
 )
-
+messages = [
+    HumanMessage(content="Hi! I'm Ryan."),
+    AIMessage(content=" Hi Ryan! It's nice to meet you. How can I assist you today? Is there something specific you would like to talk about or ask for help with? I'm here to help with answers, guidance, and helpful information on a wide range of topics. Let me know how I can help, and we can get started!"),
+    #Yes, this is the mistral response after the previous commit lol
+    HumanMessage(content="What's my name?"),
+]
 # Send a simple message to the model
-response = model.invoke([HumanMessage(content="Hi! I'm Ryan")])
+response = model.invoke(messages)
 
 # Print the model's response
 print(response.content)
